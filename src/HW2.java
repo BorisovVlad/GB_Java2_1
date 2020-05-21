e8867272454bf09a745be8c4dfc49ad1b12ab67c
BorisovVladislav <62359290+BorisovVlad@users.noreply.github.com>
2020-05-21 21:03:29 +0300

    HW2 BorisovVlad

--- src/HW2.java
+++ src/HW2.java
@@ -1,16 +1,18 @@
 import java.util.Arrays;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class HW2 {
     Scanner in = new Scanner(System.in);
 
     public static void main(String[] args) throws MyArraySizeException, MyArrayDataException {
-        String [][] arr = new String[1][2];
-        String x = "123452321";
-        for (int i = 0; i < arr.length ; i++) {
+        String[][] arr = new String[4][4];
+        String x = "1";
+        for (int i = 0; i < arr.length; i++) {
+            x = "0";
             for (int j = 0; j < arr[i].length; j++) {
                 arr[i][j] = x;
-                x += "в";
+                x = x + i;
             }
         }
 
@@ -19,47 +21,43 @@ public class HW2 {
         }
         int firstParameter = arr.length;
         int secondParameter = arr[arr.length - 1].length;
-        if (firstParameter > 4){
+        if (firstParameter > 4) {
             throw new MyArraySizeException("First parameter in array more than 4");
-        } else if (secondParameter > 4){
+        } else if (secondParameter > 4) {
             throw new MyArraySizeException("Second parameter in array more than 4");
         }
 
-        int[][] y = classForParseStringArrayToInt(arr);
-        for (int i = 0; i < y.length; i++) {
-            System.out.println(Arrays.toString(y[i]));
-        }
-
+        int y = classForParseStringArrayToInt(arr);
+            System.out.println(y);
     }
 
-    public static int[][] classForParseStringArrayToInt(String arr[][]) throws MyArrayDataException {
-        int [][] integerArray = new int[arr.length][arr[arr.length - 1].length];
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr[i].length; j++) {
-                String str;
-                char[] arrayToCheck = arr[i][j].toCharArray();
-                for (int k = 0; k < arrayToCheck.length; k++) {
-                    if (arrayToCheck[k] < '0' || arrayToCheck[k] > '9'){
-                        throw new MyArrayDataException("\nВ ячейке с индексами " + i + " : " + j + " имеется нечисловой символ, приведение которого не возможно.");
+        public static int classForParseStringArrayToInt (String arr[][]) throws MyArrayDataException {
+            int[][] integerArray = new int[arr.length][arr[arr.length - 1].length];
+            int sum = 0;
+            for (int i = 0; i < arr.length; i++) {
+                for (int j = 0; j < arr[i].length; j++) {
+                    String str;
+                    char[] arrayToCheck = arr[i][j].toCharArray();
+                    for (int k = 0; k < arrayToCheck.length; k++) {
+                        if (arrayToCheck[k] < '0' || arrayToCheck[k] > '9') {
+                            throw new MyArrayDataException("\nВ ячейке с индексами " + i + " : " + j + " имеется нечисловой символ, приведение которого не возможно.");
+                        }
                     }
+                    integerArray[i][j] = Integer.parseInt(arr[i][j]);
+                    sum += integerArray[i][j];
                 }
-
-                integerArray[i][j] = Integer.parseInt(arr[i][j]);
+            }
+            return sum;
+        }
+        public static class MyArrayDataException extends Exception {
+            public MyArrayDataException(String message) {
+                super(message);
             }
         }
-        return integerArray;
-    }
-    public static class MyArrayDataException extends Exception{
-        public MyArrayDataException(String message) {
-            super(message);
+
+        public static class MyArraySizeException extends Exception {
+            public MyArraySizeException(String message) {
+                super(message);
+            }
         }
-    }
-
-    public static class MyArraySizeException extends Exception{
-        public MyArraySizeException(String message) {
-            super(message);
-        }
-    }
-
-
 }
